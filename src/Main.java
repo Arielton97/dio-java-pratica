a45b67b926485c9183f88223b49428a6e83bfc88
Arielton97 <arieltonribeiro97@gmail.com>
2026-02-13 10:49:55 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+    /**
+     *  Tipo de ingresso (meia entrada ou ingresso família)
+     *  valor do ingresso
+     *  nome do filme
+     *  dublado ou legendado
+     *
+     *  Retorna o valor do ingresso para cada tipo de ingresso
+     *  meia entrada de 50% do valor do ingresso normal
+     *  ingresso familia multiplica o valor do ingresso normal pelo número de pessoas da família, e
+     *  fornecer um desconto de 5% quando o número de pessoas for maior que 3.
+     */
+}
